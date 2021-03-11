436204d385ec81790a5a3cd86e0832df7800080b
Shob8Sayala <shob8sayala@gmail.com>
2021-03-12 04:05:01 +0530

    first pf

---
+++ precache-manifest.da78730f976b4543162d040762cac5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf52b1dac9765b7934e9aaa61deba55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e99129e190f75782fddd",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99129e190f75782fddd",
+    "url": "/static/js/main.6aa303d7.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
